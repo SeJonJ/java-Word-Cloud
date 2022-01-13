2df125906c02b529e68a00457f9062b2afe9cdda
SeJonJ <jsj@hjproject.iptime.org>
2022-01-13 19:41:15 +0900

    쿠키 사용한 로그인 기능 구현 && 회원 정보 수정, 로그아웃 버튼 생성 && buttion 때문에 css 수정 -> readme 참고

--- src/main/java/HJproject/Hellospring/Controller/HomeController.java
+++ src/main/java/HJproject/Hellospring/Controller/HomeController.java
@@ -1,21 +1,64 @@
 package HJproject.Hellospring.Controller;
 
 
+import HJproject.Hellospring.domain.member.Member;
+import HJproject.Hellospring.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-
-    @GetMapping("/") // 가장 기본 페이지 의미, 즉 루트 페이지 위치
-    public String home(){
-        return "newspringhome"; // springhome.html 을 찾아서 연다.
-    }
-
-//    @GetMapping("login") // 로그인 페이지 get 메소드로 받으면
-//    public String login(){
-//        return "members/login"; // login.html 을 찾아서 연다
+    private final MemberRepository memberRepository;
+//    @GetMapping("/") // 가장 기본 페이지 의미, 즉 루트 페이지 위치
+//    public String home(){
+//        return "newspringhome"; // springhome.html 을 찾아서 연다.
 //    }
 
+    @GetMapping("/")
+    // @CookieValue 를 통해서 쿠키값을 가져올 수 있다.
+    // required = true 의 경우 쿠키값이 없으면 접속 불가능, false 는 없어도 접근 가능
+    public String LoginHome(@CookieValue(name = "memberCode", required = false) Long memberCode, Model model) {
+
+        // 로그인 안했을 시
+        if(memberCode == null){
+            System.out.println("memberCode : "+memberCode);
+            return "newspringhome";
+        }
+
+        /* 로그인 시도 시 */
+        // loginMember 가 Optional 로 감싸져있기 때문에 .get() 으로 한번 가져와야 안에있는 값을 가져올 수 있음
+        Optional<Member> loginMember = memberRepository.findByCode(memberCode);
+
+        // 로그인 할 때 가져오는 Code 값이 null 인 경우
+        if(loginMember.get().getCode() == null){
+            System.out.println("getCode : "+ loginMember.get().getCode());
+            return "newspringhome";
+        }
+
+        model.addAttribute("member", loginMember.get());
+        System.out.println("로그인 성공");
+        return "newspringhome_login";
+    }
+
+
+
+    @PostMapping("/logout")
+    // 쿠키를 삭제하려면 쉽게 그냥 쿠키 생명주기, 시간을 0으로 만들어 버리면 됨
+    public String logout(HttpServletResponse httpServletResponse){
+        Cookie cookie = new Cookie("memberCode", null);
+        cookie.setMaxAge(0);
+        httpServletResponse.addCookie(cookie);
+        return "redirect:/";
+    }
 }
--- src/main/java/HJproject/Hellospring/Controller/LoginComtroller.java
+++ src/main/java/HJproject/Hellospring/Controller/LoginComtroller.java
@@ -10,6 +10,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
@@ -25,7 +27,8 @@ public class LoginComtroller {
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute LoginForm form, BindingResult bindingResult) {
+    /* HttpServletResponse : 아래의 쿠키값을 생성 후 클라이언트에게 보낼때 response 에 넣어서 보내야함 */
+    public String login(@ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse httpServletResponse) {
         if (bindingResult.hasErrors()) {
             return "members/login";
 
@@ -34,13 +37,17 @@ public class LoginComtroller {
         Member LoginMember = loginService.login(form.getLoginid(), form.getLoginpw());
 
         if (LoginMember == null) { // login 메서드에서 던져주는 값이 null 이면 로그인 실패
-            bindingResult.reject("login Fail", "아이디 또는 비밀번호가 맞지 않습니다");
+//            bindingResult.reject("login Fail", "아이디 또는 비밀번호가 맞지 않습니다");
             System.out.println("로그인 실패");
             return "members/login";
         }
 
         // null 이외의 값 즉 member 객체라면 로그인 성공 처리
-        System.out.println("로그인 성공");
+
+        // 쿠키에 시간 정보를 주지 않았기 때문에 세션 쿠키로 인식됨 -> 브라우저 종료시 모두 종료
+        Cookie CookieCode = new Cookie("memberCode", String.valueOf(LoginMember.getCode()));
+        httpServletResponse.addCookie(CookieCode);
+        System.out.println("쿠키 정보 전달 완료 : "+ CookieCode);
         return "redirect:/";
     }
 
